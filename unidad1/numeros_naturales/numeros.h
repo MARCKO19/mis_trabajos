43c3e4e6092a9353fd07c5eb4f3665f1464369c5
Marco_Antonio_Noh_Noh <marco.nohnoh@itsva.edu.mx>
2018-09-20 15:06:46 -0500

    actividad

---
+++ unidad1/numeros_naturales/numeros.h
@@ -0,0 +1,22 @@
+#ifndef NUMEROS_H
+#define NUMEROS_H
+
+
+class numeros
+{
+    public:
+        numeros();
+
+        float sucesor();
+        float antecesor();
+        float suma();
+        float diferencia();
+        void operacionmenor();
+         void igual();
+         void cero(); // EL CERO Y ES CERO SE USAN EN ESTA MISMA FORMULA
+    protected:
+
+    private:
+};
+
+#endif // NUMEROS_H
